bd9cd1563239ce16566e06a736b56bb009e577a1
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-08-13 02:35:52 +0800

    fix: path

--- flake.nix
+++ flake.nix
@@ -3,13 +3,13 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = {nixpkgs, ...}: let
-    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
-  in {
-    devShells = forAllSystems (system: {
-      default = pkgs.callPackage ./shell.nix {};
-    });
-  };
+  outputs = { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [ "aarch64-linux" "x86_64-linux" ];
+    in
+    {
+      devShells = forAllSystems (system: {
+        default = nixpkgs.legacyPackages.${system}.callPackage ./shell.nix { };
+      });
+    };
 }
