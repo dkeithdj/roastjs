23f3ecd3bbc216ea8cdc361ebfb17c406eed5a26
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-08-13 02:34:02 +0800

    feat: fix

---
+++ app.py
@@ -0,0 +1,107 @@
+import os
+
+import google.generativeai as genai
+
+# from claudette import Client, models, contents
+import uvicorn
+from dotenv import load_dotenv
+from fasthtml.common import (
+    H1,
+    Body,
+    Button,
+    Div,
+    FastHTML,
+    Form,
+    Group,
+    Input,
+    Link,
+    Script,
+    Title,
+    picolink,
+)
+
+load_dotenv()
+
+genai.configure(api_key=os.environ["API_KEY"])
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+# Set up the app, including daisyui and tailwind for the chat component
+tlink = (Script(src="https://cdn.tailwindcss.com"),)
+dlink = Link(
+    rel="stylesheet",
+    href="https://cdn.jsdelivr.net/npm/daisyui@4.11.1/dist/full.min.css",
+)
+app = FastHTML(hdrs=(tlink, dlink, picolink))
+
+# Set up a chat model (https://claudette.answer.ai/)
+# cli = Client(models[0])
+sp = """You are a helpful and concise assistant."""
+messages = []
+
+
+# Chat message component (renders a chat bubble)
+def ChatMessage(msg):
+    bubble_class = (
+        "chat-bubble-primary" if msg["role"] == "user" else "chat-bubble-secondary"
+    )
+    chat_class = "chat-end" if msg["role"] == "user" else "chat-start"
+    return Div(
+        Div(msg["role"], cls="chat-header"),
+        Div(msg["content"], cls=f"chat-bubble {bubble_class}"),
+        cls=f"chat {chat_class}",
+    )
+
+
+# The input field for the user message. Also used to clear the
+# input field after sending a message via an OOB swap
+def ChatInput():
+    return Input(
+        type="text",
+        name="msg",
+        id="msg-input",
+        placeholder="Type a message",
+        cls="input input-bordered w-full",
+        hx_swap_oob="true",
+    )
+
+
+# The main screen
+@app.route("/")
+def get():
+    page = Body(
+        H1("Chatbot Demo"),
+        Div(
+            *[ChatMessage(msg) for msg in messages],
+            id="chatlist",
+            cls="chat-box h-[73vh] overflow-y-auto",
+        ),
+        Form(
+            Group(ChatInput(), Button("Send", cls="btn btn-primary")),
+            hx_post="/",
+            hx_target="#chatlist",
+            hx_swap="beforeend",
+            cls="flex space-x-2 mt-2",
+        ),
+        cls="p-4 max-w-lg mx-auto",
+    )
+    return Title("Chatbot Demo"), page
+
+
+# Handle the form submission
+@app.post("/")
+def post(msg: str):
+    messages.append({"role": "user", "content": msg})
+    response = model.generate_content("Write a story about an AI and magic")
+    print(response.text)
+    # r = cli(messages, sp=sp)  # get response from chat model
+    # messages.append({"role": "assistant", "content": contents(r)})
+    return (
+        ChatMessage(messages[-2]),  # The user's message
+        ChatMessage(messages[-1]),  # The chatbot's response
+        ChatInput(),
+    )  # And clear the input field via an OOB swap
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
--- flake.nix
+++ flake.nix
@@ -1,22 +1,15 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarc
-     h64-linux" "aarch64-darwin"];
+  outputs = {nixpkgs, ...}: let
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
   in {
-    devShells = forAllSystems (system: let
-    in {
-      default = pkgs.${system}.mkShellNoCC {
-        packages = with pkgs.${system}; [
-          python311
-        ];
-      };
+    devShells = forAllSystems (system: {
+      default = pkgs.callPackage ./shell.nix {};
     });
   };
 }
